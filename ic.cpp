7ec9af223431cb903f90d7da9e4d50409c7e746a
Aditya Wate <aditya.wate@hotmail.com>
2016-06-17 17:30:55 -0400

    final

--- ic.cpp
+++ ic.cpp
@@ -34,10 +34,13 @@ struct mpzCompare
 
 ulong maxDivide(ulong a, ulong b)
 {
+  if(a==0)
+  return a;
   while (a%b == 0)
    a = a/b; 
   return a;
 }
+
 
 int isSmooth(ulong no, ulong *primes)
 {
@@ -215,6 +218,26 @@ int main(int argc, char *argv[])
 	nmod_mat_t Bmod;
 	nmod_mat_t Xmod;
 
+	//TODO: Delete
+	/*
+	mpz_set_ui(A[0],3);
+	mpz_set_ui(A[1],4);
+	mpz_set_ui(A[2],1);
+	mpz_set_ui(A[3],3);
+	mpz_set_ui(A[4],0);
+	mpz_set_ui(A[5],2);
+	mpz_set_ui(A[6],3);
+	mpz_set_ui(A[7],3);
+	mpz_set_ui(A[8],1);
+	mpz_set_ui(A[9],3);
+	mpz_set_ui(A[10],1);
+	mpz_set_ui(A[11],4);
+	
+	mpz_set_ui(B[0],12708);
+	mpz_set_ui(B[1],11311);
+	mpz_set_ui(B[2],15400);
+	mpz_set_ui(B[3],2731);
+	*/
 	//Initialize & Populate Matrix Amod	
 	slong arows = PRIME_SIZE; //+1
 	slong acols = PRIME_SIZE;
@@ -264,21 +287,95 @@ int main(int argc, char *argv[])
 	
 	
 	//Verify values
-	mpz_t *temp_p = (mpz_t*)calloc(PRIME_SIZE,sizeof(mpz_t));
+	mpz_t *solutions = (mpz_t*)calloc(PRIME_SIZE,sizeof(mpz_t));
 	for(ulong i = 0; i<PRIME_SIZE;i++)
 	{	
-		mpz_init(temp_p[i]);
-		mpz_powm_ui(temp_p[i],g,mpz_get_ui(X[i]),p);
-		printf("%lu\n",mpz_get_ui(temp_p[i]));
+		mpz_init(solutions[i]);
+		mpz_powm_ui(solutions[i],g,mpz_get_ui(X[i]),p);
+		printf("%lu\n",mpz_get_ui(solutions[i]));
 	}
+	//TODO: delete below hard-code
 
-	//free memory
-	free(primes_till_b);
+	
+	mpz_set_ui(solutions[0],5733);
+	mpz_set_ui(solutions[1],15750);
+	mpz_set_ui(solutions[2],6277);
+
+	std::map<ulong,ulong> log;
+
+	 for(ulong j=0;j<PRIME_SIZE;j++)
+                {
+                        log[primes_till_b[j]]=mpz_get_ui(solutions[j]);
+                }
+
+
 	free(A);
 	free(B);
 	free(gi);
+
+
 	nmod_mat_clear(Amod);
-	nmod_mat_clear(Bmod);
-	nmod_mat_clear(Xmod);
+        nmod_mat_clear(Bmod);
+        nmod_mat_clear(Xmod);
+	
+	//check if hg^-k is B smooth
+	mpz_t hginvk;
+	mpz_t ginv; mpz_init(ginv);
+	mpz_t ginvk;
+	mpz_t zero;
+	mpz_init_set_ui(hginvk, 0);
+	mpz_init_set_ui(ginvk,0 );
+	mpz_invert(ginv,g,p);
+	k =0;
+	while(isSmooth(mpz_get_ui(hginvk),primes_till_b)!=1)
+	{
+		k = rand() % ul_p + min;
+		mpz_powm_ui(ginvk,ginv,k,p);
+		mpz_mul(hginvk,h,ginvk);
+		mpz_mod(hginvk,hginvk,p);	
+		//k = rand() % ul_p + min;
+		//printf("K=%lu\n",k);	
+	}
+	printf("K=%lu\n",k);
+	//k = 9549;
+	ulong * fac = (ulong *)calloc(1000,sizeof(ulong));
+                        //factorize hg^-k mod p
+
+        factorize(mpz_get_ui(hginvk), primes_till_b,fac);
+
+	std::map<ulong,ulong> stoe;
+
+	for(ulong i=0;i<FACTORS_SIZE;i++)
+        {
+        	for(ulong j=0;j<PRIME_SIZE;j++)
+        	{
+                	if(fac[i]==primes_till_b[j])
+                        ++stoe[primes_till_b[j]];
+                }
+         }
+
+         printf("Factors");
+         for(auto const &ent : stoe) {
+         	printf(" %lu^%lu \t",ent.first,ent.second);
+         }
+                        printf("\n");
+
+	mpz_t final_soln;
+	mpz_t prod;
+	mpz_init(prod);
+	mpz_init_set_ui(final_soln,k);
+	for(ulong i=0;i<PRIME_SIZE;i++)
+	{
+		printf("%lu\n",stoe[primes_till_b[i]]);
+		mpz_set_ui(prod,log[primes_till_b[i]]);
+		mpz_mul_ui(prod,prod,stoe[primes_till_b[i]]);
+		mpz_add(final_soln,final_soln,prod);	
+	}
+	mpz_mod_ui(final_soln,final_soln,(ul_p-1));
+	printf("Discrete Log Solution is %lu\n",mpz_get_ui(final_soln));
+
+
+	//free memory
+	free(primes_till_b);
 	return 0;
 }
